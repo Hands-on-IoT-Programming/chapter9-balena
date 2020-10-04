e26502e4de6f1b5e66654f903e2d39e8f9a605d5
Bernardo Ronquillo <bernardo.ronquillo@universaldx.com>
2020-10-04 13:09:42 +0200

    Set up credentials secret in NodeRED read from .env file (included in .gitginore)

--- nodered/app/start.sh
+++ nodered/app/start.sh
@@ -10,7 +10,7 @@ echo "${TIMEZONE}" > /etc/timezone
 # Source ENV variables from .env file
 set -o allexport; source .env; set +o allexport
 echo 'The NodeRED secret key is' $SECRETKEY
-# DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
-#    npm run flow9
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
-    node node_modules/node-red/red.js --settings=./settings.js flows/flow9-balena.json
+   npm run flow9
+#DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
+#    node node_modules/node-red/red.js --settings=./settings.js flows/flow9-balena.json
