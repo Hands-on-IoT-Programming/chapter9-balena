b8c13935ce54dd615d294068c0eef7bf7d09c016
Bernardo Ronquillo <bernardo.ronquillo@universaldx.com>
2020-10-04 20:18:07 +0200

    Clarify commands of start.sh

--- nodered/app/start.sh
+++ nodered/app/start.sh
@@ -6,11 +6,12 @@ echo "Setting time zone to ${TIMEZONE=UTC}"
 ln -fs /usr/share/zoneinfo/${TIMEZONE=UTC} /etc/localtime
 echo "${TIMEZONE}" > /etc/timezone
 
-# Start app
 # Source ENV variables from .env file
 set -o allexport; source .env; set +o allexport
 echo 'The NodeRED secret key is' $SECRETKEY
+
+# Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
    npm run flow9
-#DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
-#    node node_modules/node-red/red.js --settings=./settings.js flows/flow9-balena.json
+# export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+# node node_modules/node-red/red.js --settings=./settings.js flows/flow9-balena.json
